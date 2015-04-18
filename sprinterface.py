bb531fec90e8b639e224dd8bf9e0902a12217ada
Sean Sheedy <sean@phonedeveloper.com>
2015-04-18 18:55:34 -0400

    first login commit

--- sprinterface.py
+++ sprinterface.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
+def login():
+  return render_tamplate('login.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
