9648c30aceb4ed1a93ee59b89bf67adb6d6e97c4
Sean Sheedy <sean@phonedeveloper.com>
2015-04-18 21:05:53 -0400

    (work in progress)

--- sprinterface.py
+++ sprinterface.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+from config import config
+import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-  return render_tamplate('login.html')
+def login_form():
+  return render_template('login.html')
+
+@app.route('/login', methods = ['POST'])
+def do_login():
+  data = request.form
+  username = request.form['username']
+  password = request.form['password']
+  payload = {'userName': username, 'password': password}
+  response = requests.post(config['securedb_authenticate_url'], 
+                          auth=(config['securedb_user'], 
+                          config['securedb_pass']), 
+                          data=payload)
+  if (response.status_code != 200):
+    print response.status_code
+    return render_template('login_failed.html')
+  else:
+    return "woot!" # response.content
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080, debug=True)
